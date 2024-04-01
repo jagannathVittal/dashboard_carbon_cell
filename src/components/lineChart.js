a642a7b273aafa4b028f8365cce46caa09163e60
jagannathVittal <jagannathvittal18@gmail.com>
2024-04-01 11:25:05 +0530

    Initial Graph Chart Layout

---
+++ src/components/lineChart.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const LineChart = ({ data }) => {
+    return (
+
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-header-title">
+                    Conversions
+                </h4>
+            </div>
+            <div class="card-body">
+                <div class="chart">
+                    <h1>Test</h1>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default LineChart
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,24 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import Graph from "../components/grah";
+import { getGraphData } from "../api/helper";
+import Spinner from "../components/spinner";
 const Home = () => {
+    const [data, setData] = useState([])
+    const [spinner, setSpinner] = useState([])
+    useEffect(() => {
+        callGraphInfo()
+    }, [])
+    const callGraphInfo = async () => {
+        setSpinner(true)
+        const res = await getGraphData()
+        setData(res?.data)
+        setSpinner(false)
+    }
     return (
-        <div>
-            <h1>This is HomePage</h1>
+        <div className="container-fluid">
+            <Spinner show={spinner}>
+                <Graph data={data} />
+            </Spinner>
         </div>
     )
 }
