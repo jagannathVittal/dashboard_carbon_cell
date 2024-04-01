0605d13cb154f32744dde23a2f0d1e5cd9c61aa3
jagannathVittal <jagannathvittal18@gmail.com>
2024-04-01 22:47:32 +0530

    Implimented Crypto Currency Card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from "./layout/admin";
 import Home from "./pages/home";
 import Asset from "./pages/asset"
+import Trade from "./pages/trade";
 function App() {
   return (
     <Router>
@@ -10,7 +11,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/asset" element={<Asset />} />
-          <Route path="/trade" element={<Home />} />
+          <Route path="/trade" element={<Trade />} />
           <Route path="/wallet" element={<Home />} />
         </Route>
       </Routes>
--- src/pages/trade.js
+++ src/pages/trade.js
@@ -1,22 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Graph from "../components/graph";
-import { getGraphData, getCryptoCurrencyData } from "../api/helper";
+import { getCryptoCurrencyData } from "../api/helper";
 import Spinner from "../components/spinner";
 import BitCoinCard from "../components/bitCoinCard";
-const Home = () => {
-    const [data, setData] = useState([])
+const Trade = () => {
     const [spinner, setSpinner] = useState([])
     const [bitData, setBitData] = useState([])
     useEffect(() => {
-        callGraphInfo()
         callCryptoCurrencyInfo()
     }, [])
-    const callGraphInfo = async () => {
-        setSpinner(true)
-        const res = await getGraphData()
-        setData(res?.data)
-        setSpinner(false)
-    }
 
     const callCryptoCurrencyInfo = async () => {
         setSpinner(true)
@@ -28,13 +19,11 @@ const Home = () => {
     return (
         <div className="container-fluid">
             <Spinner show={spinner}>
-                <Graph data={data} />
-                <hr />
                 <BitCoinCard data={bitData} />
             </Spinner>
         </div>
     )
 }
 
-export default Home
+export default Trade
 
